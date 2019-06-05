f72539c39a3665ee40ce8590a6600be3b0dfa84c
Roppe Pauline <roppepauline@gmail.com>
2019-06-05 11:26:17 +0200

    ajout sur github

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ import 'bootstrap';
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+//importe les images du fichier
+import imgs from "./img/*.jpg";
+
+//modifie le style css
+var cats = document.querySelector("#cat");
+cats.style.height="500px";
+cats.style.width="500px";
+cats.style.objectFit="cover";
+
+//
+let i=2;
+document.getElementById("cat").src = imgs['cat'+1];
+
+//fait défiler les 12 images avec une intervale de 5 secondes
+//et recommence quand la boucle est finie
+setInterval( function(){
+  document.getElementById("cat").src = imgs['cat'+i];
+   i ++;
+  if(i > 12){
+    i = 1;
+  }
+}, 5000);
